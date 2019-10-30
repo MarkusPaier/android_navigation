e155cfb787fc6be7300c5b9c2279e5635bb74c37
Markus Paier <markus.paier@AXTESYS.local>
2019-10-30 10:55:12 +0100

    Update navigation and add safe-agrs

--- ColorMyViews/app/src/main/java/com/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/colormyviews/MainActivity.kt
@@ -3,6 +3,8 @@ package com.colormyviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController  = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun isLocalVoiceInteractionSupported(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
